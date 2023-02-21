e201f4e75c7a9ea069f6df5878c4d5764c060047
Andre Pradhana <andre.pradhana@gmail.com>
2023-02-20 20:00:39 -0800

    Update windows installation on the ci.

--- ci/install_windows.sh
+++ ci/install_windows.sh
@@ -3,6 +3,6 @@
 set -ex
 
 vcpkg update
-vcpkg install zlib libpng openexr tbb gtest blosc glfw3 glew python3 \
+vcpkg install zlib libpng openexr tbb gtest blosc -v 1.18.1 glfw3 glew python3 \
     boost-iostreams boost-any boost-uuid boost-interprocess boost-algorithm pybind11 \
     --clean-after-build
