46ad5db6681e69083c5950bff45204e27b65966f
apradhana <andre.pradhana@gmail.com>
2023-06-02 11:11:09 -0700

    VDB Example Fluid hello world.

---
+++ openvdb_examples/vdb_example_fluids/main.cc
@@ -0,0 +1,19 @@
+// Copyright Contributors to the OpenVDB Project
+// SPDX-License-Identifier: MPL-2.0
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <openvdb/openvdb.h>
+#include <openvdb/util/logging.h>
+
+
+namespace openvdb_example_fluids {
+
+} // namespace openvdb_example_fluids
+
+int
+main(int argc, char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+}
