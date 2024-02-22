6b47c3b959228a649adb910443d55895d0299c71
Ivo Kondapaneni <ivo.kondapaneni@autodesk.com>
2024-02-22 18:25:24 +0100

    Converting reader definition to be able to load half-grids from stored float-grids

--- openvdb/openvdb/tree/InternalNode.h
+++ openvdb/openvdb/tree/InternalNode.h
@@ -2417,6 +2417,9 @@ InternalNode<ChildT, Log2Dim>::readTopology(std::istream& is, bool fromHalf)
     const ValueType background = (!io::getGridBackgroundValuePtr(is) ? zeroVal<ValueType>()
         : *static_cast<const ValueType*>(io::getGridBackgroundValuePtr(is)));
 
+    // converting reader for reading grid values
+    auto& convertingReader = io::ConvertingReader<ValueType>::get(is);
+
     mChildMask.load(is);
     mValueMask.load(is);
 
@@ -2429,7 +2432,7 @@ InternalNode<ChildT, Log2Dim>::readTopology(std::istream& is, bool fromHalf)
                 child->readTopology(is);
             } else {
                 ValueType value;
-                is.read(reinterpret_cast<char*>(&value), sizeof(ValueType));
+                convertingReader.read(is, value);
                 mNodes[i].setValue(value);
             }
         }
