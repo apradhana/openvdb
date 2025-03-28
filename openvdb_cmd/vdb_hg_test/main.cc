4a31386af60361759cafeafa01f539f60f025248
apradhana <andre.pradhana@gmail.com>
2025-03-27 22:20:39 -0700

    Add hg test.

---
+++ openvdb_cmd/vdb_hg_test/main.cc
@@ -0,0 +1,19 @@
+// Copyright Contributors to the OpenVDB Project
+// SPDX-License-Identifier: Apache-2.0
+//
+/// @file main.cc
+///
+/// @brief Simple ray tracer for OpenVDB volumes
+///
+/// @note This is intended mainly as an example of how to ray-trace
+/// OpenVDB volumes.  It is not a production-quality renderer.
+
+#include <openvdb/openvdb.h>
+
+#include <iostream>
+
+int
+main(int argc, char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+}
