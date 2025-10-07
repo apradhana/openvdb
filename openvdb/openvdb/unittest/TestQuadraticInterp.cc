659d98112edef064151490faecc220f06cebb6aa
apradhana <andre.pradhana@gmail.com>
2025-10-07 00:31:30 -0700

    WIP: Add TestQuadraticInterp and TestVolumeToSpheres.
    Signed-off-by: apradhana <andre.pradhana@gmail.com>

--- openvdb/openvdb/unittest/TestQuadraticInterp.cc
+++ openvdb/openvdb/unittest/TestQuadraticInterp.cc
@@ -16,7 +16,7 @@ namespace {
 template<typename T> inline const T Tolerance;
 template<> inline const float Tolerance<float> = 1.e-5f;
 template<> inline const double Tolerance<double> = 1.e-5f;
-template<> inline const typename openvdb::Half Tolerance<openvdb::Half> = 0.00097656;
+template<> inline const typename openvdb::Half Tolerance<openvdb::Half> = static_cast<typename openvdb::Half>(0.00097656f);
 
 }
 
@@ -41,7 +41,16 @@ protected:
     static void executeTest(const GridPtr&, const TestVal*, size_t numVals);
 
     /// Initialize an arbitrary ValueType from a scalar.
-    static inline ValueT constValue(double d) { return ValueT(d); }
+    static inline ValueT constValue(double d) {
+#ifdef __clang__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wimplicit-float-conversion"
+#endif
+        return ValueT(d);
+#ifdef __clang__
+#pragma GCC diagnostic pop
+#endif
+    }
 
     /// Compare two numeric values for equality within an absolute tolerance.
     static inline bool relEq(const ValueT& v1, const ValueT& v2)
